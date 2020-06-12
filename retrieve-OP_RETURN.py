779b4d531771274f1239b6b2bc5abb34ffc599c8
Alexandre Teles <alexandre.teles@ufba.br>
2020-06-12 15:36:40 -0300

    Update retrieve-OP_RETURN.py

--- retrieve-OP_RETURN.py
+++ retrieve-OP_RETURN.py
@@ -23,40 +23,27 @@
 # THE SOFTWARE.
 
 
-import sys, string
+import sys
+import string
 from OP_RETURN import *
 
+if len(sys.argv) < 2:
+    sys.exit(
+        '''Usage:
+        python retrieve-OP_RETURN.py <hash> <testnet (optional)>'''
+    )
 
-if len(sys.argv)<2:
-	sys.exit(
-'''Usage:
-python retrieve-OP_RETURN.py <ref> <testnet (optional)>'''
-	)
+ref = sys.argv[1]
 
-ref=sys.argv[1]
-
-if len(sys.argv)>2:
-	testnet=bool(sys.argv[2])
+if len(sys.argv) > 2:
+    testnet = bool(sys.argv[2])
 else:
-	testnet=False
+    testnet = False
 
-results=OP_RETURN_retrieve(ref, 1, testnet)
+results = OP_RETURN_check(ref, testnet)
 
 if 'error' in results:
-	print('Error: '+results['error'])
-	
-elif len(results):
-	for result in results:
-		print("Hex: ("+str(len(result['data']))+" bytes)\n"+OP_RETURN_bin_to_hex(result['data'])+"\n")
-		print("ASCII:\n"+re.sub(b'[^\x20-\x7E]', b'?', result['data']).decode('utf-8')+"\n")
-		print("TxIDs: (count "+str(len(result['txids']))+")\n"+"\n".join(result['txids'])+"\n")
-		print("Blocks:"+("\n"+("\n".join(map(str, result['heights'])))+"\n").replace("\n0\n", "\n[mempool]\n"))
-		
-		if 'ref' in result:
-			print("Best ref:\n"+result['ref']+"\n")
-
-		if 'error' in result:
-			print("Error:\n"+result['error']+"\n")
-
+    print('Error: ' + results['error'])
 else:
-	print("No matching data was found")
+    for result, decoded in results:
+        print(f'OP_RETURN output: {result} | Decoded as: {decoded}')
